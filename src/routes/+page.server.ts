8b003e2ed7cb770f6756343707354d72dc5d2de1
Michal Dyczkowski <themich4@gmail.com>
2024-03-14 19:17:19 +0100

    Added pokemon grid

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import { MainClient } from 'pokenode-ts';
+
+export async function load() {
+	const api = new MainClient();
+
+	const { pokemon_entries } = await api.game.getPokedexByName('paldea');
+
+	const pokemons = pokemon_entries.map((pokemon) => {
+		const urlSplit = pokemon.pokemon_species.url.split('/');
+
+		return {
+			...pokemon,
+			id: urlSplit[urlSplit.length - 2]
+		};
+	});
+
+	return {
+		pokemons
+	};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	export let data;
+
+	console.log({ data });
+</script>
+
+<div class="flex min-w-screen min-h-screen justify-center items-center bg-blue-300 p-8">
+	<div class="container grid grid-cols-5 gap-4 max-w-[800px]">
+		{#each data.pokemons as pokemon}
+			<div
+				class="border rounded-lg bg-blue-200/20 border-blue-200/80 gap-2 flex items-center p-4 flex-col justify-between"
+			>
+				<div class="uppercase font-semibold tracking-tight text-center text-nowrap">
+					{pokemon.pokemon_species.name}
+				</div>
+				<div class="rounded-full bg-blue-200/80 w-24 h-24">
+					<img
+						src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+						alt={pokemon.pokemon_species.name}
+					/>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
